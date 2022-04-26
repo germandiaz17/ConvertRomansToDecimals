a5ea4b280094b2f09a753df4ed4700200affcfcc
germandiaz17 <germanrodrigo.2003@htmail.com>
2022-04-25 20:48:10 -0500

    cambios gr

--- src/features/RomToDec.js
+++ src/features/RomToDec.js
@@ -1,14 +1,15 @@
 const CharToInt = (caracter) => {
-    switch(caracter) {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
-        default : return -1;
-    }
+
+    const romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    } 
+    return romans[caracter]
 };
 
 const RomToDecFunc = (romano) => {
